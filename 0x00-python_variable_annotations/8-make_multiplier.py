4cc71e316e87c5a7fd7e53d15e7300fd2ec602da
otega07 <okiriotega@gmail.com>
2024-08-08 15:00:01 +0100

    third commit

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """a type-annotated function make_multiplier that takes a float multiplier as argument and
-returns a function that multiplies a float by multiplier."""
+returns a function that multiplies a float by multiplier.
+"""
 from typing import Callable
 
 
